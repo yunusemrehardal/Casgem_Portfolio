87216e0af5ec311cafb15218e19b5222784ea146
Yunus Emre Hardal <hardaalyunusemre@gmail.com>
2023-07-13 00:30:37 +0300

    Add project files.

---
+++ Casgem_Portfolio/Controllers/PortfolioController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Casgem_Portfolio.Models.Entities;
+
+namespace Casgem_Portfolio.Controllers
+{
+    public class PortfolioController : Controller
+    {
+        CasgemPortfolioEntities1 db = new CasgemPortfolioEntities1();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public PartialViewResult PartialHead()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialNavbar()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialFeature()
+        {
+            ViewBag.featureTittle = db.TblFeature.Select(x => x.FeatureTittle).FirstOrDefault();
+            ViewBag.featureDescription = db.TblFeature.Select(x => x.FeatureDescription).FirstOrDefault();
+            ViewBag.featureImageUrl = db.TblFeature.Select(x => x.FeatureImageUrl).FirstOrDefault();
+            return PartialView();
+        }
+        public PartialViewResult MyResume()
+        {
+            var values = db.TblResume.ToList();
+            return PartialView(values);
+        }
+        public PartialViewResult PartialStatistic()
+        {
+            ViewBag.totalService = db.TblService.Count();
+            ViewBag.totalMessage = db.TblMessage.Count();
+            ViewBag.totalThanksMessage = db.TblMessage.Where(x=>x.MessageSubject=="Teşekkür").Count();
+            ViewBag.happyCustomer = 12;
+            return PartialView();
+        }
+        [HttpGet]
+        public FileResult Download()
+        {
+            byte[] fileBytes = System.IO.File.ReadAllBytes(this.Server.MapPath("/Data/CV_EN_HARDAL.pdf"));
+            string fileName = "CV_EN_HARDAL.pdf";
+            return File(fileBytes, System.Net.Mime.MediaTypeNames.Application.Octet, fileName);
+        }
+    }
+}
---
+++ Casgem_Portfolio/Controllers/ProjectController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Casgem_Portfolio.Models.Entities;
+
+namespace Casgem_Portfolio.Controllers
+{
+    public class ProjectController : Controller
+    {
+        CasgemPortfolioEntities1 db = new CasgemPortfolioEntities1();
+        public ActionResult Index()
+        {
+            var values = db.TblProject.ToList();
+            return View(values);
+        }
+        [HttpGet]
+        public ActionResult AddProject()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddProject(TblProject p)
+        {
+            db.TblProject.Add(p);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult DeleteProject(int id)
+        {
+            var value = db.TblProject.Find(id);
+            db.TblProject.Remove(value);
+            db.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+        [HttpGet]
+        public ActionResult UpdateProject(int id)
+        {
+            var value = db.TblProject.Find(id);
+            return View(value);
+        }
+        [HttpPost]
+        public ActionResult UpdateProject(TblProject p)
+        {
+            var value = db.TblProject.Find(p.ProjectID);
+            value.ProjectName = p.ProjectName;
+            value.ProjectDescription = p.ProjectDescription;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
